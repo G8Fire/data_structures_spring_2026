d5ca541faba6b3f5bc772cc7074cb34a67cdf17c
G8Fire <dbb062006@gmail.com>
2026-01-15 12:21:48 -0600

    Add Dog class with methods for data and behavior

--- main.cpp
+++ main.cpp
@@ -1,12 +1,45 @@
 // Name: Dalton Bishop
-// Description:
+// Description: Practice GitHub and creating a dog class
 // 1/25/2026
 
 #include <iostream>
 
 using namespace std;
 
+class Dog {
+
+private:
+	string name;
+	int age;
+	string breed;
+public:
+	void setData(string dogName, int dogAge, string dogBreed) {
+		name = dogName;
+		age = dogAge;
+		breed = dogBreed;
+	}
+	void bark() {
+		cout << "Woof! Woof!" << endl;
+	}
+	void displayInfo() {
+		cout << "Name: " << name << ", Age: " << age << ", Breed: " << breed << endl;
+	}
+	string changeName(string newName) {
+		name = newName;
+		return name;
+	}
+
+};
+
 int main() {
-	cout << "Hello, World!" << endl;
+
+	class Dog myDog;
+	myDog.setData("Buddy", 3, "Golden Retriever");
+	myDog.bark();
+	myDog.displayInfo();
+	myDog.changeName("Max");
+	myDog.displayInfo();
+	// End of program
+
 	return 0;
 }
